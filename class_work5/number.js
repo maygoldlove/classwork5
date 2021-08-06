7b52d7a4a389b69ed046c51eb0434e0b5091356f
maygoldlove <85343054+maygoldlove@users.noreply.github.com>
2021-08-06 03:56:18 -0700

    Add files via upload

---
+++ class_work5/number.js
@@ -0,0 +1,16 @@
+const num = document.form.value
+constsubmitBtn= document.querySelector('#submit');
+submitBtn.addEventlistner('click', function (e) {
+
+if (num == Number)
+{
+alert( 'Input Correct');
+}
+else {
+    alert('Input Wrong')
+
+}
+} )
+
+
+constpassword=
